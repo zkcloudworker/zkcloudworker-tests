a59e5d718b2dc537e66ff098263874ebcc4fb31f
mike dupont <mike.dupont@introspector.local>
2024-10-23 09:46:25 -0400

    adding other git forks to test

---
+++ list-versions.py
@@ -0,0 +1,16 @@
+from git import Repo
+import time
+import datetime
+now = time.time()
+r = Repo(".")
+for x in r.remotes:
+    for url in x.urls:
+        url = url.replace("git@github.com:","git+https://github.com/")
+        for z in x.refs:
+            if (z.is_valid()):
+                cd = time.gmtime(z.commit.committed_date)
+                diff = now -z.commit.committed_date
+                diff2 = datetime.timedelta(seconds=diff)
+                days = diff2.days
+                if days < 30:
+                    print(str(url)+"#" +str(z.commit)+ ", #" + time.asctime(cd) + " " + str(days) + " " + z.name)
